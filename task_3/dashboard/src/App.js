dca80799d3d8ec59a2d3512c0c9d023571626aa1
Taylor Joyner <taylor.joyner93@me.com>
2024-05-06 15:11:31 -0500

    through task 4

---
+++ task_3/dashboard/src/App.js
@@ -0,0 +1,27 @@
+import './App.css';
+import holbertonLogo from './holberton-logo.jpg';
+import { getFullYear, getFooterCopy } from './utils';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="App-header">
+        <img src={holbertonLogo} alt="Holberton Logo" />
+        <h1>School dashboard</h1>
+      </div>
+      <div className="App-body">
+        <p>Login to access the full dashboard</p>
+        <label htmlFor="email">Email:</label>
+        <input type="email" id="email" name="email" />
+        <label htmlFor="password">Password:</label>
+        <input type="password" id="password" name="password" />
+        <button>OK</button>
+      </div>
+      <div className="App-footer">
+        <p>{getFooterCopy(true)} {getFullYear()}</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
