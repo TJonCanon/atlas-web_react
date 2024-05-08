2c22417d14ebeb343f49b94b54c76ca5295877c8
TJonCanon <taylor.joyner93@me.com>
2024-05-08 16:09:25 -0500

    react props

---
+++ react-props/task_1/dashboard/src/App/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import holbertonLogo from './holberton-logo.jpg';
+import { getFullYear, getFooterCopy } from '../utils/utils';
+
+function App() {
+  return (
+    <>
+      <Notifications />
+      <Header />
+      <Login />
+      <Footer />
+    </>
+  );
+}
+
+export default App;;
