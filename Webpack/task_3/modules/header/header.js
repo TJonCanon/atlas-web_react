440d06a296b777f71a93daaa75c5bbe971566135
TJonCanon <taylor.joyner93@me.com>
2024-05-08 14:32:16 -0500

    Updates

--- Webpack/task_3/modules/header/header.js
+++
@@ -1,7 +0,0 @@
-import $ from 'jquery';
-$(document).ready(function() {
-    $('body').prepend('<header></header>');
-    $('header').append('<div id="logo"></div>');
-    $('header').append('<h1>Holberton Dashboard</h1>');
-    console.log('Init header');
-  });
