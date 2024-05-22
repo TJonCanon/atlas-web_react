b3f38a2af13696934ff6db35dc90c50e596dc6cc
TJonCanon <taylor.joyner93@me.com>
2024-05-22 16:37:46 -0500

    task 3

---
+++ 0x08_react_redux_action_creator_normalizr/task_3/dashboard/src/App/AppContext.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export const user = {
+  email: '',
+  password: '',
+  isLoggedIn: false,
+};
+
+export const logOut = () => {};
+
+export const AppContext = React.createContext({
+  user,
+  logOut,
+});
